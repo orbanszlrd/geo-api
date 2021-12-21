8e1713258ebdf4312e417f28e619ead52470c8c8
Orban Szilard <orbanszlrd@gmail.com>
2021-12-21 15:56:30 +0100

    Create sql script for the creation of the country table

--- src/main/java/com/orbanszlrd/geo/country/Country.java
+++ src/main/java/com/orbanszlrd/geo/country/Country.java
@@ -1,7 +1,6 @@
 package com.orbanszlrd.geo.country;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
