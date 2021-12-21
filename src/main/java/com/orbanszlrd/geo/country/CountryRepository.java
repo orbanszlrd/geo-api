a3af238de2078b622e4a6319feb9d484768bec99
Orban Szilard <orbanszlrd@gmail.com>
2021-12-21 12:53:23 +0100

    Create the Country repository

---
+++ src/main/java/com/orbanszlrd/geo/country/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.orbanszlrd.geo.country;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+    Optional<Country> findByName(String name);
+}
